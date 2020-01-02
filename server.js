a0e9a7f95ac79eb6e1e37a9fe7a6178adb211124
jayakrishna-g <gandhamallajayakrishna@hotmail.com>
2020-01-02 10:53:48 +0530

    path to 'path'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
+const path=require('path')
 const port = process.env.PORT||3000
-const path=require(path)
 app.use(express.static(__dirname + '/dist/ngproject'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
